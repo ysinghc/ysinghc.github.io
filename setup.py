26cc2ab6d1880b998085b10c31404ca203409a1a
Yuvraj Singh Chauhan <133221941+ysinghc@users.noreply.github.com>
2024-11-07 12:15:47 +0000

    removing all the old files

--- setup.py
+++
@@ -1,7 +0,0 @@
-import subprocess as sbp
-
-sbp.run(["sudo", "apt", "install", "git", "-y"])
-sbp.run(["git", "clone", "https://github.com/ysinghc/ysinghc.github.io.git"])
-sbp.run(["git", "config", "--global", "user.name", "\"klarab\""])
-sbp.run(["git", "config", "--global", "user.email", "\"imv-in\""])
-
